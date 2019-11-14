3c1a02dbf40ac43283ddf6757e295b1c2898cfe9
Peter-cmd <2849308036@qq.com>
2019-11-14 23:14:09 +0800

    提交Client2.java和HanFeiZi.java文件,内容是将监听者放到被监听者内部.

---
+++ Client2.java
@@ -0,0 +1,11 @@
+package test02;
+
+public class Client2 {
+    public static void main(String[] args){
+        //定义韩非子
+        HanFeiZi hanFeiZi = new HanFeiZi();
+        //韩非子开始活动
+        hanFeiZi.haveBreakfast();
+        hanFeiZi.haveFun();
+    }
+}
---
+++ HanFeiZi2.java
@@ -0,0 +1,23 @@
+﻿package test02;
+import test01.IHanFeiZi;
+import test01.LiSi;
+import test01.ILiSi;
+
+public class HanFeiZi2 implements IHanFeiZi {
+
+    //声明李斯
+    private ILiSi lisi = new LiSi();
+
+    //韩非子要吃饭了
+    public void haveBreakfast(){
+        System.out.println("韩非子:开始吃饭了……");
+        //通知李斯
+        this.lisi.update("韩非子在吃饭.");
+    }
+
+    //韩非子要娱乐了
+    public void haveFun(){
+        System.out.println("韩非子:开始娱乐了……");
+        this.lisi.update("韩非子在娱乐");
+    }
+}
