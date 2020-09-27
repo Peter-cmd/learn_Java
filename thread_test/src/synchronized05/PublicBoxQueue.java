08f9f366b8c614bac4590d4e143c902def848933
Deut-hangd <deuthangd@163.com>
2020-09-27 23:44:25 +0800

    上传thread_test文件,内容是: 此次提交的文件是我对synchronized的学习,即利用synchronized锁类的静态方法和类的.class,以及锁类的实例方法和this的方式实现线程同步.还有阻塞队列BlockingQueue的练习(模拟生产者和消费者的生产和消费过程).

---
+++ thread_test/src/synchronized05/PublicBoxQueue.java
@@ -0,0 +1,58 @@
+package synchronized05;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PublicBoxQueue {
+    public static void main(String[] args){
+        BlockingQueue publicBoxQueue = new LinkedBlockingQueue(5);
+        
+        Thread pro = new Thread(new ProducerQueue(publicBoxQueue), "生产者");
+        Thread con = new Thread(new ConsumerQueue(publicBoxQueue), "消费者");
+
+        pro.start();
+        con.start();
+
+    }
+
+}
+
+class ProducerQueue implements Runnable{
+    private final BlockingQueue proQueue;
+
+    public ProducerQueue(BlockingQueue proQueue){
+        this.proQueue = proQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++){
+            System.out.println(Thread.currentThread().getName() + "生产的苹果编号为" + i);
+            try {
+                proQueue.put(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class ConsumerQueue implements Runnable{
+    private final BlockingQueue conQueue;
+
+    public ConsumerQueue(BlockingQueue conQueue){
+        this.conQueue = conQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++){
+            try {
+                System.out.println(Thread.currentThread().getName() + "消费的苹果编号为" + conQueue.take());
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
