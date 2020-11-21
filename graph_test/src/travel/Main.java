4e665aec326c339b92ccaa84e40773d250fe415f
Deut-hangd <deuthangd@163.com>
2020-11-21 23:24:57 +0800

    上传graph_test文件,内容是: 小明目前在做一份毕业旅行的规划。打算从北京出发，分别去若干个城市，然后再回到北京，每个城市之间均乘坐高铁，且每个城市只去一次。由于经费有限，希望能够通过合理的路线安排尽可能的省一些路上的花销。给定一组城市和每对城市之间的火车票的价钱，找到每个城市只访问一次并返回起点的最小车费花销。

---
+++ graph_test/src/travel/Main.java
@@ -0,0 +1,45 @@
+package travel;
+
+import java.util.Scanner;
+public class Main{
+    private static int findTheLeastCost(int n, int[][] src){
+        int m = (int)Math.pow(2, n - 1);
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++){
+            dp[i][0] = src[i][0];
+        }
+
+        for (int j = 1; j < m; j++){
+            for (int i = 0; i < n; i++){
+                dp[i][j] = Integer.MAX_VALUE;
+                if (((j >> (i - 1)) & 1) == 1){
+                    continue;
+                }
+
+                for (int k = 1; k < n; k++){
+                    if (((j >> (k - 1)) & 1) == 1){
+                        dp[i][j] = Math.min(dp[i][j], src[i][k] + dp[k][j ^ (1 << (k - 1))]);
+                    }
+                }
+            }
+        }
+        return dp[0][m - 1];
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            int n = in.nextInt();
+            int[][] src = new int[n][n];
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    src[i][j] = in.nextInt();
+                }
+            }
+            int result = findTheLeastCost(n, src);
+            System.out.println(result);
+        }
+
+    }
+}
