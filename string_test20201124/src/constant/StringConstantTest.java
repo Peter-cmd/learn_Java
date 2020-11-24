c65c2cc0f50c791dcd4827eb8e73ce274c23b481
Deut-hangd <deuthangd@163.com>
2020-11-24 23:56:03 +0800

    上传string_test20201124文件,内容是: String 类的 intern方法的练习: 当调用 intern 方法时, 若字符串常量池中已有 相同内容的对象, 返回该对象的引用; 反之, 创建 一个相同内容的对象并加入字符串常量池, 返回 对象的引用.

---
+++ string_test20201124/src/constant/StringConstantTest.java
@@ -0,0 +1,26 @@
+package constant;
+
+public class StringConstantTest {
+
+    public static void main(String[] args){
+//        String a = "abc";
+//        String b = "abc";
+//        System.out.println(a == b);
+//
+//        String c = new String("cde");
+//
+////        String e = c.intern();
+////
+//        String f = c.intern();
+//        String d = "cde";
+//
+//        String e = new String("cde");
+//
+//        System.out.println(d == f);
+        String a = "ab";
+        String b = "a" + "b";
+        String c = "b";
+        String d = "a" + c;
+
+    }
+}
