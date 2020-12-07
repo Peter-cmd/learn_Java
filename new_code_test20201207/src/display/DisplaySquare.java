cebf07dc452b6099f4bf847d8be970f1fb0d9ace
Deut-hangd <deuthangd@163.com>
2020-12-07 23:58:23 +0800

    上传new_code_test20201207文件, 内容是: 以正方形的形式打印某个字符.

---
+++ new_code_test20201207/src/display/DisplaySquare.java
@@ -0,0 +1,38 @@
+package display;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class DisplaySquare {
+
+    private static void displaySquare(int n, String c) {
+        if (n == 0 || " ".equals(c)) {
+            return;
+        }
+
+        int halfN = (int)Math.round((float)n / 2);
+
+        for (int i = 0; i < halfN; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0) || (i == halfN - 1)) {
+                    System.out.print(c);
+                }else if (j == 0 || j == n - 1){
+                    System.out.print(c);
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            String c = in.next();
+            displaySquare(n, c);
+        }
+    }
+}
