0afdb8bb8a74659901da9ff5d19d2c1372abc30d
Deut-hangd <deuthangd@163.com>
2020-12-16 23:41:19 +0800

    上传aop_test20201216文件, 文件内容是: spring boot aop练习.

---
+++ aop_test20201216/src/main/java/com/example/aop_test/controller/AddController.java
@@ -0,0 +1,22 @@
+package com.example.aop_test.controller;
+
+import com.example.aop_test.service.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddController {
+
+    @Autowired
+    ServiceImpl serviceImpl;
+
+    @RequestMapping("/add")
+    public void add(int a1, int a2) {
+        serviceImpl.add(a1, a2);
+    }
+
+}
---
+++ aop_test20201216/src/main/java/com/example/aop_test/service/IService.java
@@ -0,0 +1,8 @@
+package com.example.aop_test.service;
+
+public interface IService {
+
+    void add(int a1, int a2);
+
+    void sub();
+}
